3bde29c9450ae28c47ca55921bb3e1ed6c4b73cf
sacultang <sacultang@gmail.com>
2022-03-28 16:51:03 +0900

    game css

---
+++ main.js
@@ -0,0 +1,9 @@
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+
+
+function initGame(){
+  // 벌레와 당근을 생성한뒤 field에 추가해줌
+  console.log(fieldRect)
+}
+initGame()
